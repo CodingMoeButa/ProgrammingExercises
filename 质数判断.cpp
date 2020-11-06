34bf305f840c35a5fb06a4ddd1f46ddd0fc142f2
Ricky Lee <molesg@yeah.net>
2020-11-06 08:52:52 +0800

    Create 质数判断.cpp

---
+++ 质数判断.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n < 2)
+        return 0;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+
+int main() {
+    int q,i;
+    cin>>q;
+    while (q > 0) {
+        cin>>i;
+        if (isPrime(i)) {
+            cout<<"Yes\n";
+        } else {
+            cout<<"No\n";
+        }
+        q--;
+    }
+}
