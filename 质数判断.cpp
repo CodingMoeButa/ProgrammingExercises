9adfdeeb40aa706a5facb15f7541d5c1baa309f8
Ricky Lee <molesg@yeah.net>
2020-11-06 09:13:55 +0800

    Update 质数判断.cpp

--- 质数判断.cpp
+++ 质数判断.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 bool isPrime(int n) {
     if (n < 2)
-        return 0;
+        return false;
     for (int i = 2; i * i <= n; i++)
         if (n % i == 0)
             return false;
